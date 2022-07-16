2724702945af22718abce54ed349757a03a73bbc
allaye.gana <allayegana78@gmail.com>
2022-07-15 22:04:00 -0300

    pai e filho e sniper

---
+++ src/componentes/direita/filho.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { Button, Text } from 'react-native'
+import estilo from './estilo'
+
+export default props => {
+    let x = 13
+    let y = 100
+        return (
+               <Text style={estilo.testogrande}>
+                   
+                </Text>
+                )
+}
